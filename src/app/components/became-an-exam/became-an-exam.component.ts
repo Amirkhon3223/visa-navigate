52c9c18c70a9d2947dbeedf1e2105f32f8cc23ea
Amirkhon3223 <amirich3223@outlook.com>
2024-03-18 13:15:27 +0500

    became an exam

---
+++ src/app/components/became-an-exam/became-an-exam.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-became-an-exam',
+  standalone: true,
+  imports: [],
+  templateUrl: './became-an-exam.component.html',
+  styleUrl: './became-an-exam.component.css'
+})
+export class BecameAnExamComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {SliderComponent} from "../../components/slider/slider.component";
+import { BecameAnExamComponent } from '../../components/became-an-exam/became-an-exam.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    SliderComponent
+    SliderComponent,
+    BecameAnExamComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
